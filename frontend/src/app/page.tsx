4bb46cc004c57a044818e54882f4e61fe6a31a00
VillaDev <avfonseca00@gmail.com>
2025-06-02 00:31:11 -0400

    fix(frontend): Delete unused vars

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,10 @@
-import Header from "@components/Header";
 import Hero from "@components/Hero";
-import Shorten from "@components/Shorten";
 import Statistics from "@components/Statistics";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <Shorten/>
       <Statistics/>
     </>
   );
