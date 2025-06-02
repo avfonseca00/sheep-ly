e00dfb7af5a9484841ac9c3f8186f7d6ffe2d796
VillaDev <avfonseca00@gmail.com>
2025-06-01 22:57:54 -0400

    perf(frontend): Improve responsiveness

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -5,22 +5,22 @@ import ButtonRounded from "./ButtonRounded";
 export default function Header() {
     return (
         <header className="flex justify-center bg-white">
-            <nav className="flex justify-between w-full lg:max-w-5xl xl:max-w-7xl p-6">
-                <div className="flex gap-12 items-center">
+            <nav className="flex justify-between w-full lg:max-w-5xl xl:max-w-6xl p-6">
+                <div className="flex gap-10 items-center">
                     <a href="#" className="p-1.5">
                         <img src="/next.svg" className="w-auto h-6 bg-cy" alt="Logo"/>
                     </a>
-                    <div className="hidden md:flex gap-12">
-                        <a href="" className="text-sm">Features</a>
-                        <a href="" className="text-sm">Pricing</a>
-                        <a href="" className="text-sm">Resources</a>
+                    <div className="hidden md:flex gap-9">
+                        <a href="" className="text-sm text-grayish-violet font-bold">Features</a>
+                        <a href="" className="text-sm text-grayish-violet font-bold">Pricing</a>
+                        <a href="" className="text-sm text-grayish-violet font-bold">Resources</a>
                     </div>
                 </div>
                 <div className="flex items-center">
                     <button className="md:hidden">
                         <BiMenu size={36} className="fill-grayish-violet"/>
                     </button>
-                    <div className="md:flex hidden">
+                    <div className="md:flex gap-4 hidden ">
                         <ButtonLink>Login</ButtonLink>
                         <ButtonRounded variant="sm">Sign Up</ButtonRounded>
                     </div>
--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -1,20 +1,22 @@
 import ButtonRounded from "./ButtonRounded";
+import Shorten from "./Shorten";
 
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center w-full lg:max-w-5xl xl:max-w-7xl gap-12 pb-14">
-            <div className="flex overflow-hidden">
-                <img src="/images/illustration-working.svg" className="h-[326] ml-6 object-cover object-left" alt="" />
+        <div className="flex flex-col items-center w-full lg:max-w-5xl md:flex-row-reverse lg:overflow-visible xl:max-w-6xl md:pt-13.5 gap-12 pb-42 relative">
+            <div className="flex overflow-hidden md:overflow-visible">
+                <img src="/images/illustration-working.svg" className="h-[326] lg:overflow-visible xl:h-[482px] ml-6 object-cover object-left" alt="" />
             </div>
-            <div className="flex flex-col items-center gap-8">
-                <div className="flex flex-col px-4 gap-3">
-                    <h1 className="text-dark-violet text-[40px] font-bold leading-12 tracking-tight text-center">More than just shorter links</h1>
-                    <p className="text-grayish-violet text-lg text-center leading-7">Build your brand’s recognition and get detailed insights on how your links are performing.</p>
+            <div className="flex flex-col items-center md:items-start md:pl-6 gap-8">
+                <div className="flex flex-col md:px-0 px-4 gap-3">
+                    <h1 className="text-dark-violet text-[40px] md:text-5xl md:leading-13 lg:text-6xl lg:leading-16 md:text-left xl:text-[80px] xl:leading-22.5 font-bold leading-12 tracking-tight text-center">More than just shorter links</h1>
+                    <p className="text-grayish-violet text-lg md:text-left xl:text-[22px] xl:max-w-[550px] xl:leading-8 text-center leading-7">Build your brand’s recognition and get detailed insights on how your links are performing.</p>
                 </div>
                 <ButtonRounded variant="lg">
                     Get Started
                 </ButtonRounded>
             </div>
+            <Shorten/>
         </div>
     )
 }
