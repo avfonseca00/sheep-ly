efe3cdd943d3d842d669a2de6873d99b110045f0
VillaDev <avfonseca00@gmail.com>
2025-06-02 00:25:48 -0400

    feat(frontend): Add Statistics section and favicon svg

---
+++ frontend/src/app/components/Statistics.tsx
@@ -0,0 +1,16 @@
+type Props = {}
+
+export default function Statistics({}: Props) {
+    return (
+        <div className="flex flex-col items-center w-full bg-grayish-violet-light pt-42.5">
+            <div className=" flex flex-col w-full lg:max-w-5xl xl:max-w-6xl">
+                <div className="flex flex-col gap-4">
+                    <h3 className="text-[28px] font-bold text-center tracking-[-0.75px]">Advanced Statistics</h3>
+                    <p className="text-grayish-violet text-md text-center leading-7">
+                        Track how your links are performing across the web with our advanced statistics dashboard.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
