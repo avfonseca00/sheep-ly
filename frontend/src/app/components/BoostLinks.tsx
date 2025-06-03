62cc6fdc3ca0c57c224e6bec729808c07801fc96
VillaDev <avfonseca00@gmail.com>
2025-06-03 11:20:04 -0400

    feat(frontend): Add BoostLinks section

---
+++ frontend/src/app/components/BoostLinks.tsx
@@ -0,0 +1,12 @@
+import ButtonRounded from "./ButtonRounded";
+
+export default function BoostLinks() {
+    return (
+        <div className="flex flex-col items-center w-full gap-4 py-23 md:p-17 bg-dark-violet bg-[url(/images/bg-boost-mobile.svg)] md:bg-[url(/images/bg-boost-desktop.svg)] bg-no-repeat bg-top-right p-6 xl:px-16 xl:py-13">
+            <h4 className="text-[28px] text-white font-bold text-center tracking-[-0.75px] xl:text-[40px]">Boost your links today</h4>
+            <ButtonRounded variant="lg">
+                Get Started
+            </ButtonRounded>
+        </div>
+    )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
+import BoostLinks from "@components/BoostLinks";
 import Hero from "@components/Hero";
+import Shorten from "@components/Shorten";
 import Statistics from "@components/Statistics";
 
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Shorten/>
       <Statistics/>
+      <BoostLinks/>
     </>
   );
 }
