e3fb2cb2ec9b155206f8680d7622c9b144b76019
VillaDev <avfonseca00@gmail.com>
2025-06-06 15:50:52 -0400

    feat(frontend): Add copy to clipboard functionality

--- frontend/src/app/components/LinkCard.tsx
+++ frontend/src/app/components/LinkCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 type Props = {
@@ -7,15 +7,29 @@ type Props = {
 }
 
 export default function LinkCard({link, short}: Props) {
+
+    const [copied, setCopied] = useState<boolean>(false);
+    const copyToClipboard = async (e: React.MouseEvent<HTMLButtonElement>) => {
+
+        e.preventDefault();
+
+        try {
+            await navigator.clipboard.writeText(short);
+            setCopied(true)
+        } catch (error) {
+            setCopied(false)
+        }
+    }
+
     return (
         <div className="flex flex-col md:flex-row md:justify-between bg-white rounded-md mx-6">
-            <div className="flex items-center border-b md:border-none border-[hsl(0,0%,85%)] p-3.5">
+            <div className="flex items-center min-w-0 border-b md:border-none border-[hsl(0,0%,85%)] p-3.5">
                 <p className="text-md md:text-lg w-full overflow-hidden whitespace-nowrap overflow-ellipsis">{link}</p>
             </div>
             <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-6 p-4">
-                <p className="text-md md:text-lg w-full text-cyan ">{short}</p>
-                <Button variant='sm'>
-                    Copy
+                <a href={short} className="text-md md:text-lg w-auto text-cyan ">{short}</a>
+                <Button size='sm' className={copied ? 'bg-dark-violet':''} onClick={copyToClipboard}>
+                    {copied? 'Copied!': 'Copy'}
                 </Button>
             </div>
         </div>
