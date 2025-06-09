220c81392fb8253080aae706e99440b7d1f317ce
VillaDev <avfonseca00@gmail.com>
2025-06-09 16:48:16 -0400

    feat(frontend): Improve shorten link url page UI

--- frontend/src/app/[shortLink]/page.tsx
+++ frontend/src/app/[shortLink]/page.tsx
@@ -1,4 +1,6 @@
 import ButtonRounded from '@components/ButtonRounded';
+import Footer from '@components/Footer';
+import Header from '@components/Header';
 import axios from 'axios'
 export default async function Redirect({ params }: { params: Promise<{ shortLink: string }> }) {
 
@@ -7,10 +9,18 @@ export default async function Redirect({ params }: { params: Promise<{ shortLink
     const link = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/links/${shortLink}`)
 
     return (
-        <div className='flex items-center h-dvh'>
-            <ButtonRounded size='lg' link={link.data.originalLink}>
-                Get Link
-            </ButtonRounded>
-        </div>
+        <>
+            <Header onTop={true}/>
+            <main className="flex flex-col items-center w-full">
+                <div className='flex flex-col items-center justify-center gap-6 w-full h-[calc(100dvh-92px)]'>
+                    <img src="images/celebration.svg" alt="" className='w-full md:w-md'/>
+                    <p className="text-[16px] text-center text-very-dark-violet px-10">Congratulations! Your link is ready and you can get it from button below</p>
+                    <ButtonRounded size='lg' link={link.data.originalLink}>
+                        Get Link
+                    </ButtonRounded>
+                </div>
+            </main>
+            <Footer/>
+        </>
     )
 }
